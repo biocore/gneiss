3772e58211fe8789a674c0e5117905ba4a64c94e
Jamie Morton <jamietmorton@gmail.com>
2016-06-30 13:07:31 -0700

    ENH: Adding in balance basis with tests

---
+++ gneiss/balances.py
@@ -0,0 +1,115 @@
+from __future__ import division
+import numpy as np
+from skbio.stats.composition import clr_inv
+from collections import OrderedDict
+
+
+def _balance_basis(tree_node):
+    """ Helper method for calculating balance basis
+    """
+
+    n_tips = sum([n.is_tip() for n in tree_node.traverse()])
+    counts = _count_matrix(tree_node)
+    counts = OrderedDict([(x, counts[x])
+                          for x in counts.keys() if not x.is_tip()])
+    nds = counts.keys()
+    r = np.array([counts[n]['r'] for n in nds])
+    s = np.array([counts[n]['l'] for n in nds])
+    k = np.array([counts[n]['k'] for n in nds])
+    t = np.array([counts[n]['t'] for n in nds])
+
+    a = np.sqrt(s / (r*(r+s)))
+    b = -1*np.sqrt(r / (s*(r+s)))
+
+    basis = np.zeros((n_tips-1, n_tips))
+    for i in range(len(nds)):
+        basis[i, :] = np.array([0]*k[i] + [a[i]]*r[i] + [b[i]]*s[i] + [0]*t[i])
+    return basis, nds
+
+
+def balance_basis(tree_node):
+    """
+    Determines the basis based on tree
+
+    Parameters
+    ----------
+    treenode : skbio.TreeNode
+        Phylogenetic tree.  Must be a strictly bifurcating tree
+    Returns
+    -------
+    basis : np.array
+        Returns a set of orthonormal bases in the Aitchison simplex
+        corresponding to the phylogenetic tree. The order of the
+        basis is index by the level order of the internal nodes.
+    nodes : list, skbio.TreeNode
+        List of tree nodes indicating the ordering in the basis.
+
+    Raises
+    ------
+    ValueError
+        The tree doesn't contain two branches
+
+    Examples
+    --------
+    >>> from gneiss.balances import balance_basis
+    >>> from skbio import TreeNode
+    >>> tree = u"((b,c)a, d)root;"
+    >>> t = TreeNode.read([tree])
+    >>> basis, nodes = balance_basis(t)
+    >>> basis
+    array([[ 0.62985567,  0.18507216,  0.18507216],
+           [ 0.28399541,  0.57597535,  0.14002925]])
+
+    Notes
+    -----
+    The tree must be strictly bifurcating, meaning that
+    every internal node has exactly 2 children.
+    """
+    basis, nodes = _balance_basis(tree_node)
+    basis = clr_inv(basis)
+    return basis, nodes
+
+
+def _count_matrix(treenode):
+    nodes = list(treenode.levelorder(include_self=True))
+    # fill in the dictionary
+    counts = OrderedDict()
+    columns = ['k', 'r', 'l', 't', 'tips']
+    for n in nodes:
+        if n not in counts:
+            counts[n] = {}
+        for c in columns:
+            counts[n][c] = 0
+
+    # fill in r and l
+    for n in nodes[::-1]:
+        if n.is_tip():
+            counts[n]['tips'] = 1
+        elif len(n.children) == 2:
+            lchild = n.children[0]
+            rchild = n.children[1]
+            counts[n]['r'] = counts[rchild]['tips']
+            counts[n]['l'] = counts[lchild]['tips']
+            counts[n]['tips'] = counts[n]['r'] + counts[n]['l']
+        else:
+            raise ValueError("Not a strictly bifurcating tree!")
+
+    # fill in k and t
+    for n in nodes:
+        if n.parent is None:
+            counts[n]['k'] = 0
+            counts[n]['t'] = 0
+            continue
+        elif n.is_tip():
+            continue
+        # left or right child
+        # left = 0, right = 1
+        child_idx = 'l' if n.parent.children[0] != n else 'r'
+        if child_idx == 'l':
+            counts[n]['t'] = counts[n.parent]['t'] + counts[n.parent]['l']
+            counts[n]['k'] = counts[n.parent]['k']
+        else:
+            counts[n]['k'] = counts[n.parent]['k'] + counts[n.parent]['r']
+            counts[n]['t'] = counts[n.parent]['t']
+
+    return counts
---
+++ gneiss/tests/test_balances.py
@@ -0,0 +1,111 @@
+from __future__ import absolute_import, division, print_function
+import unittest
+import numpy as np
+import numpy.testing as npt
+from gneiss.balances import balance_basis, _count_matrix, _balance_basis
+from skbio import TreeNode
+from skbio.util import get_data_path
+
+
+class TestBalances(unittest.TestCase):
+
+    def test_count_matrix_base_case(self):
+        tree = u"(a,b);"
+        t = TreeNode.read([tree])
+        res = _count_matrix(t)
+        exp = {'k': 0, 'l': 1, 'r': 1, 't': 0, 'tips': 2}
+        self.assertEqual(res[t], exp)
+
+        exp = {'k': 0, 'l': 0, 'r': 0, 't': 0, 'tips': 1}
+        self.assertEqual(res[t[0]], exp)
+
+        exp = {'k': 0, 'l': 0, 'r': 0, 't': 0, 'tips': 1}
+        self.assertEqual(res[t[1]], exp)
+
+    def test_count_matrix_unbalanced(self):
+        tree = u"((a,b)c, d);"
+        t = TreeNode.read([tree])
+        res = _count_matrix(t)
+
+        exp = {'k': 0, 'l': 2, 'r': 1, 't': 0, 'tips': 3}
+        self.assertEqual(res[t], exp)
+        exp = {'k': 1, 'l': 1, 'r': 1, 't': 0, 'tips': 2}
+        self.assertEqual(res[t[0]], exp)
+
+        exp = {'k': 0, 'l': 0, 'r': 0, 't': 0, 'tips': 1}
+        self.assertEqual(res[t[1]], exp)
+        self.assertEqual(res[t[0][0]], exp)
+        self.assertEqual(res[t[0][1]], exp)
+
+    def test_count_matrix_singleton_error(self):
+        with self.assertRaises(ValueError):
+            tree = u"(((a,b)c, d)root);"
+            t = TreeNode.read([tree])
+            _count_matrix(t)
+
+    def test_count_matrix_trifurcating_error(self):
+        with self.assertRaises(ValueError):
+            tree = u"((a,b,e)c, d);"
+            t = TreeNode.read([tree])
+            _count_matrix(t)
+
+    def test__balance_basis_base_case(self):
+        tree = u"(a,b);"
+        t = TreeNode.read([tree])
+
+        exp_basis = np.array([[np.sqrt(1. / 2), -np.sqrt(1. / 2)]])
+        exp_keys = [t]
+        res_basis, res_keys = _balance_basis(t)
+
+        npt.assert_allclose(exp_basis, res_basis)
+        self.assertListEqual(exp_keys, list(res_keys))
+
+    def test__balance_basis_unbalanced(self):
+        tree = u"((a,b)c, d);"
+        t = TreeNode.read([tree])
+
+        exp_basis = np.array([[np.sqrt(2. / 3), -np.sqrt(1. / 6),
+                               -np.sqrt(1. / 6)],
+                              [0, np.sqrt(1. / 2), -np.sqrt(1. / 2)]
+                              ])
+        exp_keys = [t, t[0]]
+        res_basis, res_keys = _balance_basis(t)
+
+        npt.assert_allclose(exp_basis, res_basis)
+        self.assertListEqual(exp_keys, list(res_keys))
+
+    def test_balance_basis_base_case(self):
+        tree = u"(a,b);"
+        t = TreeNode.read([tree])
+        exp_keys = [t]
+        exp_basis = np.array([0.80442968, 0.19557032])
+        res_basis, res_keys = balance_basis(t)
+
+        npt.assert_allclose(exp_basis, res_basis)
+        self.assertListEqual(exp_keys, list(res_keys))
+
+    def test_balance_basis_unbalanced(self):
+        tree = u"((a,b)c, d);"
+        t = TreeNode.read([tree])
+        exp_keys = [t, t[0]]
+        exp_basis = np.array([[0.62985567, 0.18507216, 0.18507216],
+                             [0.28399541, 0.57597535, 0.14002925]])
+
+        res_basis, res_keys = balance_basis(t)
+
+        npt.assert_allclose(exp_basis, res_basis)
+        self.assertListEqual(exp_keys, list(res_keys))
+
+    def test_balance_basis_large1(self):
+        fname = get_data_path('large_tree.nwk',
+                              subfolder='data')
+        t = TreeNode.read(fname)
+        exp_basis = np.loadtxt(
+            get_data_path('large_tree_basis.txt',
+                          subfolder='data'))
+        res_basis, res_keys = balance_basis(t)
+        npt.assert_allclose(exp_basis, res_basis)
+
+
+if __name__ == "__main__":
+    unittest.main()
