252ca0fd8ce3924efc5a4c5f77d60fdd6fbe2191
Jamie Morton <jamietmorton@gmail.com>
2016-06-30 21:50:30 -0700

    Forgot __init__.py :/

---
+++ gneiss/__init__.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, gneiss development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from __future__ import absolute_import, division, print_function
+
+
+__version__ = "0.0.1"
