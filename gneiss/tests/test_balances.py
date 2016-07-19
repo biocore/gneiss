38b64c856f6e186f67cb1aa0854dcdb44dc9d801
Jamie Morton <jamietmorton@gmail.com>
2016-07-19 10:29:48 -0700

    GPL license

--- gneiss/tests/test_balances.py
+++ gneiss/tests/test_balances.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, gneiss development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
 from __future__ import absolute_import, division, print_function
 import unittest
 import numpy as np
