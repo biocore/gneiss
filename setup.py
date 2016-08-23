e4c184644b3713627ac7cc2c317651f895762c96
Jamie Morton <jamietmorton@gmail.com>
2016-08-23 10:21:57 -0700

    Fixing licenses

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------------
 # Copyright (c) 2016--, gneiss development team.
 #
-# Distributed under the terms of the Modified BSD License.
+# Distributed under the terms of the GPLv3 License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
@@ -36,7 +36,7 @@ if USE_CYTHON:
 
 classes = """
     Development Status :: 3 - Alpha
-    License :: OSI Approved :: BSD License
+    License :: OSI Approved :: GPLv3 License
     Topic :: Software Development :: Libraries
     Topic :: Scientific/Engineering
     Topic :: Scientific/Engineering :: Bio-Informatics
