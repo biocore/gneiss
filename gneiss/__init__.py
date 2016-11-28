66744d0be8a4ec04a6f9e00c478407bfa3e506b1
Jamie Morton <jamietmorton@gmail.com>
2016-11-28 13:12:00 -0800

    clean up for sphinx

--- gneiss/__init__.py
+++
@@ -1,9 +0,0 @@
-# ----------------------------------------------------------------------------
-# Copyright (c) 2016--, gneiss development team.
-#
-# Distributed under the terms of the GPLv3 License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-# ----------------------------------------------------------------------------
-
-__version__ = "0.1.4"
