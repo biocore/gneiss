4a6c9893411dbba6fdb7dace4416ab4a2717a44e
Jamie Morton <jamietmorton@gmail.com>
2016-12-22 19:13:08 -0700

    Adding dendrogram heatmap

---
+++ gneiss/plot/__init__.py
@@ -0,0 +1,22 @@
+"""
+Plotting functions (:mod:`gneiss.plot`)
+===============================================
+
+.. currentmodule:: gneiss.plot
+
+This module contains plotting functionality
+
+Functions
+---------
+
+.. autosummary::
+   :toctree: generated/
+
+   heatmap
+
+"""
+
+from .heatmap import heatmap
+
+
+__all__ = ["heatmap"]
