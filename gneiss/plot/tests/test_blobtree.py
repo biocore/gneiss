e9ec4c611f8a99bc7dd7e142c12b4faa2213a422
Jamie Morton <jamietmorton@gmail.com>
2017-01-11 08:45:16 -0800

    Adding test changes

--- gneiss/plot/tests/test_blobtree.py
+++ gneiss/plot/tests/test_blobtree.py
@@ -25,5 +25,15 @@ class BlobTreeTest(unittest.TestCase):
         self.assertTrue(os.path.exists(self.fname))
         self.assertTrue(os.path.getsize(self.fname) > 0)
 
+    def test_no_collapsed_nodes(self):
+        st = TreeNode.read(get_data_path(self.newick))
+        tr, ts = diamondtree(st, breadth_scaling=6, depth_scaling=30,
+                             cladecolors={'y5': '#FF0000', 'y18': '#0000FF'},
+                             bgcolors={'y29': '#00FF00'})
+
+        tr.render(file_name=self.fname, tree_style=ts)
+        self.assertTrue(os.path.exists(self.fname))
+        self.assertTrue(os.path.getsize(self.fname) > 0)
+
 if __name__ == "__main__":
     unittest.main()
