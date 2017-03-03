8f311c102ee7cb3b0b0fe305aa070be53415c786
Jamie Morton <jamietmorton@gmail.com>
2017-03-03 11:21:36 -0800

    Adding header

--- gneiss/plot/tests/test_plot.py
+++ gneiss/plot/tests/test_plot.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, gneiss development team.
+#
+# Distributed under the terms of the GPLv3 License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
 import unittest
 import os
 import shutil
