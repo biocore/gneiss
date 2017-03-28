14c1dfffe1a2b86a4d78645e0ae0d869f404e818
Jamie Morton <jamietmorton@gmail.com>
2017-03-28 16:27:45 -0600

    ENH: Adding in q2 cli for ilr

---
+++ gneiss/stats/_stats.py
@@ -0,0 +1,50 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, gneiss development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+import pandas as pd
+import skbio
+from skbio.stats.composition import ilr
+
+from q2_composition.plugin_setup import Composition, Balance
+from q2_types.feature_table import FeatureTable
+from q2_types.tree import Phylogeny, Rooted, Unrooted
+from qiime2.plugin import Str, Metadata
+from gneiss.plugin_setup import plugin
+from gneiss.balances import balance_basis
+
+
+def ilr_transform(table: pd.DataFrame, tree: skbio.TreeNode) -> pd.DataFrame:
+    basis, _ = balance_basis(tree)
+    balances = ilr(table.values, basis)
+    in_nodes = [n.name for n in tree.levelorder() if not n.is_tip()]
+    return pd.DataFrame(balances,
+                        columns=in_nodes,
+                        index=table.index)
+
+
+plugin.methods.register_function(
+    function=ols_regression,
+    inputs={'table': FeatureTable[Composition],
+            'tree': Phylogeny[Rooted | Unrooted]},
+    outputs=[('balances', FeatureTable[Balance])],
+    name='Isometric Log-ratio Transform',
+    input_descriptions={
+        'table': ('The feature table containing the samples in which '
+                  'the ilr transform will be performed.'),
+        'tree': ('A hierarchy of feature identifiers that defines the '
+                 'partitions of features.  Each tip in the hierarchy'
+                 'corresponds to the feature identifiers in the table. '
+                 'This tree can contain tip ids that are not present in '
+                 'the table, but all feature ids in the table must be '
+                 'present in this tree.  This assumes that all of the '
+                 'internal nodes in the tree have labels.')
+    },
+    parameter_descriptions={},
+    output_descriptions={'balances': ('The resulting balances from the '
+                                      'ilr transform.')},
+    description="Calculate balances given a hierarchy."
+)
