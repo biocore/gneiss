32396130a3061d0abfda9524dcc4ca9a87a5778d
Jamie Morton <jamietmorton@gmail.com>
2017-06-16 17:41:31 -0600

    Updating docs for principal balances

--- gneiss/cluster/_pba.py
+++ gneiss/cluster/_pba.py
@@ -18,8 +18,7 @@ from scipy.cluster.hierarchy import linkage
 
 def correlation_linkage(X, method='ward'):
     r"""
-    Principal Balance Analysis using Hierarchical Clustering
-    based on correlationity.
+    Hierarchical Clustering based on proportionality.
 
     The hierarchy is built based on the correlationity between
     any two pairs of features.  Specifically the correlation between
@@ -32,6 +31,8 @@ def correlation_linkage(X, method='ward'):
     are said to be highly correlation. A hierarchical clustering is
     then performed using this correlation as a distance metric.
 
+    This can be useful for constructing principal balances.
+
     Parameters
     ----------
     X : pd.DataFrame
@@ -43,7 +44,7 @@ def correlation_linkage(X, method='ward'):
     Returns
     -------
     skbio.TreeNode
-        Tree generated from principal balance analysis.
+        Tree for constructing principal balances.
 
     References
     ----------
@@ -79,8 +80,7 @@ def correlation_linkage(X, method='ward'):
 
 
 def rank_linkage(r, method='average'):
-    r""" Principal Balance Analysis using Hierchical Clustering on
-    feature ranks.
+    r""" Hierchical Clustering on feature ranks.
 
     The hierarchy is built based on the rank values of the features given
     an input vector `r` of ranks. The distance between two features :math:`x`
@@ -92,6 +92,8 @@ def rank_linkage(r, method='average'):
     Where :math:`r(x)` is the rank of the features.  Hierarchical clustering is
     then performed using :math:`d(x, y)` as the distance metric.
 
+    This can be useful for constructing principal balances.
+
     Parameters
     ----------
     r : pd.Series
@@ -102,7 +104,7 @@ def rank_linkage(r, method='average'):
     Returns
     -------
     skbio.TreeNode
-        Tree generated from principal balance analysis.
+        Tree for constructing principal balances.
 
     Examples
     --------
@@ -129,8 +131,7 @@ def rank_linkage(r, method='average'):
 
 def gradient_linkage(X, y, method='average'):
     r"""
-    Principal Balance Analysis using Hierarchical Clustering
-    on known gradient.
+    Hierarchical Clustering on known gradient.
 
     The hierarchy is built based on the values of the samples
     located along a gradient.  Given a feature :math:`x`, the mean gradient
@@ -154,6 +155,8 @@ def gradient_linkage(X, y, method='average'):
     A hierarchical clustering is then performed using :math:`d(x, y)` as
     the distance metric.
 
+    This can be useful for constructing principal balances.
+
     Parameters
     ----------
     X : pd.DataFrame
@@ -167,7 +170,7 @@ def gradient_linkage(X, y, method='average'):
     Returns
     -------
     skbio.TreeNode
-        Tree generated from principal balance analysis.
+        Tree for constructing principal balances.
 
     See Also
     --------
@@ -212,13 +215,12 @@ def random_linkage(n):
     Returns
     -------
     skbio.TreeNode
-        Random tree
+        Random tree for constructing principal balances.
 
     Examples
     --------
     >>> from gneiss.cluster import random_linkage
     >>> tree = random_linkage(10)
-
 
     Notes
     -----
