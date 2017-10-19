73b4485c78a03474dbe79f9b57f69c87fdf45e22
Jamie Morton <jmorton@eng.ucsd.edu>
2017-10-18 17:51:48 -0700

    Update __init__.py

--- gneiss/__init__.py
+++ gneiss/__init__.py
@@ -6,4 +6,4 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-__version__ = "0.4.2"
+__version__ = "0.4.2-dev"
