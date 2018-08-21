f761527d3c0abff4df93f20e1b9e3a025c0edb29
Lisa <lisa.roth5@gmail.com>
2018-08-21 13:25:52 -0700

    Update _composition.py

--- gneiss/composition/_composition.py
+++ gneiss/composition/_composition.py
@@ -11,16 +11,15 @@ from skbio.stats.composition import ilr
 from gneiss.balances import balance_basis
 from gneiss.util import match_tips
 
-  """ Performs isometric logratio (ilr) transformation on feature-table.
-      Zeros must first be removed from the table (e.g. add-pseudocount).
-      Creates a new table with balances (groups of features) that distinguish samples. 
-  """
-
 def ilr_transform(table: pd.DataFrame, tree: skbio.TreeNode) -> pd.DataFrame:
     _table, _tree = match_tips(table, tree)
     basis, _ = balance_basis(_tree)
     balances = ilr(_table.values, basis)
     in_nodes = [n.name for n in _tree.levelorder() if not n.is_tip()]
+  """ Performs isometric logratio (ilr) transformation on feature-table.
+      Zeros must first be removed from the table (e.g. add-pseudocount).
+      Creates a new table with balances (groups of features) that distinguish samples. 
+  """
     return pd.DataFrame(balances,
                         columns=in_nodes,
                         index=table.index)
