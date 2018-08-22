a8a41aef7b3914e6241f171d868f9c8705cd8926
Lisa <lisa.roth5@gmail.com>
2018-08-22 09:34:21 -0700

    Update _composition.py

--- gneiss/composition/_composition.py
+++ gneiss/composition/_composition.py
@@ -12,14 +12,30 @@ from gneiss.balances import balance_basis
 from gneiss.util import match_tips
 
 def ilr_transform(table: pd.DataFrame, tree: skbio.TreeNode) -> pd.DataFrame:
+    """Performs isometric logratio (ilr) transformation on feature-table.
+    This creates a new table with balances (groups of features) that distinguish samples. 
+    Zeros must first be removed from the table (e.g. add-pseudocount).
+
+    Parameters
+    -----------
+    table : pd.DataFrame
+        Dataframe of the feature table where rows correspond to samples and columns
+        are features. The values within the table must be positive and nonzero.
+    tree : skbio.TreeNode
+        A tree relating all of the features to balances or log-contrasts (hierarchy).
+        This tree must be bifurcating (i.e. has exactly 2 nodes). The internal nodes 
+        of the tree will be renamed. 
+
+    Returns
+    --------
+    balances : pd.DataFrame
+         Balances calculated from the feature table. Balance represents the log ratio 
+         of subchildren values below the specified internal node.
+    """
     _table, _tree = match_tips(table, tree)
     basis, _ = balance_basis(_tree)
     balances = ilr(_table.values, basis)
     in_nodes = [n.name for n in _tree.levelorder() if not n.is_tip()]
-  """ Performs isometric logratio (ilr) transformation on feature-table.
-      Zeros must first be removed from the table (e.g. add-pseudocount).
-      Creates a new table with balances (groups of features) that distinguish samples. 
-  """
     return pd.DataFrame(balances,
                         columns=in_nodes,
                         index=table.index)
