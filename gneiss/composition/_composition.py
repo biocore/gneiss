4741cdfe7fc9a882a0616caa600b1832c770be8e
Jamie Morton <jamietmorton@gmail.com>
2019-01-09 16:24:49 -0500

    Revert "Composition docs (#268)"
    This reverts commit 2bed3e31e3867757e85213d5ae8402a0a32c9470.

--- gneiss/composition/_composition.py
+++ gneiss/composition/_composition.py
@@ -13,31 +13,6 @@ from gneiss.util import match_tips
 
 
 def ilr_transform(table: pd.DataFrame, tree: skbio.TreeNode) -> pd.DataFrame:
-    """Performs isometric logratio (ilr) transformation on feature-table.
-
-    This creates a new table with balances (groups of features) that
-    distinguish samples. Zeros must first be removed from the table
-    (e.g. add-pseudocount). For source documentation check out:
-    https://numpydoc.readthedocs.io/en/latest/
-
-    Parameters
-    -----------
-    table : pd.DataFrame
-        Dataframe of the feature table where rows correspond to samples
-        and columns are features. The values within the table must be
-        positive and nonzero.
-    tree : skbio.TreeNode
-        A tree relating all of the features to balances or
-        log-contrasts (hierarchy). This tree must be bifurcating
-        (i.e. has exactly 2 nodes). The internal nodes of the tree
-         will be renamed.
-
-    Returns
-    --------
-    balances : pd.DataFrame
-         Balances calculated from the feature table. Balance represents
-         the log ratio of subchildren values below the specified internal node.
-    """
     _table, _tree = match_tips(table, tree)
     basis, _ = balance_basis(_tree)
     balances = ilr(_table.values, basis)
